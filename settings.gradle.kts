fe91eb07ddfdbc5f628418f0a1f1815cf1210371
keerthikaashyap <54125768+Arbitrary-2912@users.noreply.github.com>
2024-03-05 14:14:15 -0500

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS-2340-Assignment2"
+include(":app")
